663a9240d8188766f0a3524df6a58e526bba05bf
HomePC <xeeshanahmad348@gmail.com>
2025-05-03 12:34:17 +0500

    Handle jwt token in cookies for state persistence

--- backend/src/controllers/authUser.controller.js
+++ backend/src/controllers/authUser.controller.js
@@ -19,11 +19,10 @@ const signUp=async (req,res)=>{
             role:role||'user'
         })
         await user.save();
-        const token=jwt.sign({id:user._id,role:user.role},JWT_SECRET)
-        
+        // const token=jwt.sign({id:user._id,role:user.role},JWT_SECRET,{ expiresIn: '1d' })
         res.status(201).json({
             message:'user created successfully',
-            token:token,
+            // token:token,
             role:user.role
         })
     } catch (error) {
@@ -42,9 +41,14 @@ const login=async (req,res)=>{
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) throw new Error("Wrong password");
 
-        const token = jwt.sign({ id: user._id, role: user.role }, JWT_SECRET);
+        const token = jwt.sign({ id: user._id, role: user.role }, JWT_SECRET,{ expiresIn: '1d' });
 
-        res.status(200).json({
+        res.cookie("jwt", token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax",
+        maxAge: 24 * 60 * 60 * 1000
+        }).status(200).json({
              token, 
              role: user.role,
              message: "login successful"
@@ -56,4 +60,22 @@ const login=async (req,res)=>{
     }
 }
 
-export {login,signUp}
+const getLoggedInUser = (req, res) => {
+    const token = req.cookies.jwt;
+    console.log(token);
+    if (!token) return res.status(401).json({ message: 'Not authenticated' });
+  
+    try {
+        console.log("token tu mil gya h")
+      const decoded = jwt.verify(token, JWT_SECRET);
+      console.log("decode b kr liya h",decoded);
+      // Optionally fetch full user details from DB here using decoded.id
+      res.status(200).json({ user: { id: decoded.id, role: decoded.role } });
+    } catch (err) {
+      res.status(401).json({ message: 'Invalid token' });
+    }
+  };
+  
+  
+
+export {login,signUp,getLoggedInUser}
