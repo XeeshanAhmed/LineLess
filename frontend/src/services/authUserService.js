73dfb87706e7d8682cc4854f4d42eb27548aa388
Hassnain-Idrees <hassnainidrees@gmail.com>
2025-05-03 18:12:48 +0500

    Integrated OTP APi at Bussiness Signup Front end

--- frontend/src/services/authUserService.js
+++ frontend/src/services/authUserService.js
@@ -12,3 +12,13 @@ export const signupUser = async (data) => {
     const response = await axios.post(`${BASE_URL}/signup`, data);
     return response.data;
   };
+
+export const sendUserOtp = async (email) => {
+  const response = await axios.post(`${API_BASE_URL}/send-otp`, { email });
+  return response.data;
+};
+
+export const verifyUserOtp = async ({ email, otp }) => {
+  const response = await axios.post(`${API_BASE_URL}/verify-otp`, { email, otp });
+  return response.data;
+};
